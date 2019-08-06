148e653a357273ea8ad54b4e777d72cb2c675727
SilverSabre92 <lczapraga@gmail.com>
2019-08-06 21:51:10 +0200

    Third Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+import sun.java2d.pipe.SpanShapeRenderer;
+
 public class TestingMain {
         public static void main (String[] args){
-            System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+            SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+            String result = simpleUser.getUsername();
+
+            if(result.equals("theForumUser")){
+                System.out.println("Test OK");
+            }else{
+                System.out.println("Error Blyat!!");
+            }
+            Calculator calculator = new Calculator(10,3);
+            int addResult = calculator.addAToB();
+            
+            if(addResult ==13){
+                System.out.println("Well done my lord");
+            }else{
+                System.out.println("Wel... this went bad");
+            }
+            int substractResult = calculator.substractAFromB();
+            
+            if(substractResult == 7){
+                System.out.println("Mision accomplished");
+            }else{
+                System.out.println ("Mission failed...");
+            }
         }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,26 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    private int a,b;
+
+    public Calculator(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+    public int getA(){
+        return this.a;
+    }
+    public int getB(){
+        return this.b;
+    }
+    public int addAToB(){
+        return a+b;
+    }
+    public int substractAFromB(){
+        return a-b;
+    }
+    @Override
+    public String toString(){
+        return a + " " + b;
+    }
+}

6a53915d5b566e24897e983ea5a14f0efa62def9
SilverSabre92 <lczapraga@gmail.com>
2019-08-15 20:14:02 +0200

    Task 7.3 - Create an ArrayList, after it create a map from selected ArrayList elements with stream

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,13 +1,57 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
 import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.person.People;
 import com.kodilla.stream.reference.FunctionalCalculator;
+
+import java.sql.SQLOutput;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        Forum forum = new Forum();
+        Map<Integer, Object> theResultForum = forum.getList().stream()
+                .filter(forumUser -> forumUser.getSex() == 'm')
+                .filter(forumUser -> forumUser.getBorn().getYear() < 1999)
+                .filter(forumUser -> forumUser.getPublicatedPosts()>1)
+                .collect(Collectors.toMap(ForumUser::getUserUniqueNumber, forumUser -> forumUser));
+
+        System.out.println("# Elements :" + theResultForum.size());
+        theResultForum.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
+
+
+        /*BookDirectory theBookDirectory = new BookDirectory();
+
+        String theResultStringOfBook = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication()> 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining("\n", "<<", ">>"));
+
+        System.out.println(theResultStringOfBook);*/
+
+/*        Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toMap(Book ::getSignature, book -> book));
+
+        System.out.println("#elements: " + theResultMapOfBooks.size());
+        theResultMapOfBooks.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);*/
+
+
+        /*ExpressionExecutor expressionExecutor = new ExpressionExecutor();
 
         System.out.println("Calculating expression with lambdas");
         expressionExecutor.executeExpression(10,5, (a,b) -> a+b);
@@ -31,7 +75,29 @@ public class StreamMain {
         beatufiedText.textToBeautify(" SO MUCH SPACE ", ((toBeBeatufied) -> (toBeBeatufied).toLowerCase() + "Chief"));
 
         System.out.println("Using stream to generate even numbers from 1 to 100");
-        NumbersGenerator.generateEven(100);
-    }
+        NumbersGenerator.generateEven(20);
+
+        People.getList().stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > 11)
+                .map(s -> s.substring(0, s.indexOf(' ') +2) + ".")
+                .filter(s -> s.substring(0,1).equals("M"))
+                .forEach(System.out::println);
+
+
+        BookDirectory bookDirectory = new BookDirectory();
+        bookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .forEach(System.out::println);
+
+        BookDirectory theBookDirectory = new BookDirectory();
+        List<Book> theResultListOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toList());
+
+        System.out.println("# elements :" + theResultListOfBooks.size());
+        theResultListOfBooks.stream()
+                .forEach(System.out::println);
+    */}
 
 }
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -0,0 +1,43 @@
+package com.kodilla.stream.forumuser;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Forum {
+    private final List<ForumUser> forumUsersList= new ArrayList<>();
+
+    public Forum(){
+        forumUsersList.add(new ForumUser(101, "JohnRatnicky", 'm', 2001, 12, 10, 15 ));
+        forumUsersList.add(new ForumUser(102, "MonikaRokicky", 'f', 1998, 10, 13, 0));
+        forumUsersList.add(new ForumUser(103, "DarrylJ", 'm', 1998, 11, 12, 10));
+        forumUsersList.add(new ForumUser(104, "Montolivo", 'm', 2005, 10, 1, 15));
+        forumUsersList.add(new ForumUser(105, "VladimirP", 'm', 1965, 2, 3, 1352));
+        forumUsersList.add(new ForumUser(106, "Margaret", 'f', 1996, 2, 13, 1526));
+        forumUsersList.add(new ForumUser(107, "Paulinho", 'm', 2005, 6, 10, 125));
+        forumUsersList.add(new ForumUser(108, "Corni", 'f', 1998, 10, 13, 256));
+        forumUsersList.add(new ForumUser(109, "Jonataninho", 'm', 1994, 4, 12, 639));
+
+    }
+    public List<ForumUser> getList(){
+        return new ArrayList<>(forumUsersList);
+    }
+}
+
+/*
+
+private final List<Book> theBookList = new ArrayList<>();
+    public BookDirectory(){
+        theBookList.add(new Book("Dylan Murphy", "Wolf of the mountains", 2003, "0001"));
+        theBookList.add(new Book("Phoebe Pearson", "Slaves of dreams", 2012, "0002"));
+        theBookList.add(new Book("Morgan Walsh", "Obliteration of heaven", 2001, "0003"));
+        theBookList.add(new Book("Aimee Murphy", "Rejecting the night", 2015, "0004"));
+        theBookList.add(new Book("Ryan Talley", "Gangsters and kings", 2007, "0005"));
+        theBookList.add(new Book("Madelynn Carson", "Unity without duty", 2007, "0006"));
+        theBookList.add(new Book("Giancarlo Guerrero", "Enemies of eternity", 2009, "0007"));
+    }
+    public List<Book> getList(){
+        return new ArrayList<>(theBookList);
+    }
+
+ */

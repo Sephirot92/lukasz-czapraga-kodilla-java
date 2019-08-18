6e384e7b0dd85e97421f6b02d0749c7a06208fe1
SilverSabre92 <lczapraga@gmail.com>
2019-08-18 23:32:42 +0200

    7.5. upgraded - deleted ArrayTask, createn static method within interface, upgraded testclass to test only interface

--- kodilla-stream/src/test/java/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/stream/array/ArrayOperationsTestSuite.java
@@ -1,24 +1,23 @@
-//package stream.array;
-//
-//import com.kodilla.stream.array.ArrayOperations;
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-//
-//public class ArrayOperationsTestSuite {
-//    @Test
-//    public void testArrayOperations() {
-//        //Given
-//        int[] simpleArray = {1,8,5,6,4,1,3,2,6,4,8,2};
-//
-//        //when
-//        ArrayOperations task = new ArrayOperations(simpleArray);
-//
-//        double testThisArray =
-//        System.out.println(testThisArray);
-//        System.out.println();
-//        //Then
-//        Assert.assertEquals(4.166666666666667, testThisArray, 0);
-//    }
-//
-//}
+package stream.array;
+
+import com.kodilla.stream.array.ArrayOperations;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class ArrayOperationsTestSuite {
+    @Test
+    public void testArrayOperations() {
+        //Given
+        int[] simpleArray = {1,8,5,6,4,1,3,2,6,4,8,2};
+
+        //when
+
+        double testThisArray =ArrayOperations.getAverage(simpleArray);
+        System.out.println(testThisArray);
+        System.out.println();
+        //Then
+        Assert.assertEquals(4.166666666666667, testThisArray, 0);
+    }
+
+}

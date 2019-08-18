7a74a0734013f3d04325f628eabbf76fa01cf6ff
SilverSabre92 <lczapraga@gmail.com>
2019-08-18 16:02:48 +0200

    7.5

---
+++ kodilla-stream/src/test/java/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,24 @@
+package stream.array;
+
+import com.kodilla.stream.array.ArrayOperations;
+import com.kodilla.stream.array.ArrayTask;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class ArrayOperationsTestSuite {
+    @Test
+    public void testArrayOperations() {
+        //Given
+        int[] simpleArray = {1,8,5,6,4,1,3,2,6,4,8,2};
+
+        //when
+        ArrayTask task = new ArrayTask();
+        double testThisArray = task.getAverage(simpleArray);
+        System.out.println(testThisArray);
+        System.out.println();
+        //Then
+        Assert.assertEquals(4.166666666666667, testThisArray, 0);
+    }
+
+}
